b0b7d8018f3f4990a180e720a3b018a1a9ab7b38
Anna Krishna <125814322+Akrishnaveni@users.noreply.github.com>
2025-01-28 07:22:31 +0530

    removed unnecessaries in reportWebVitals.js file

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,16 +1,5 @@
-import { getCLS, getFID, getFCP, getLCP, getTTFB } from "web-vitals";
-
-const reportWebVitals = (onPerfEntry) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
-  }
-};
-
-export default reportWebVitals;
+export function reportWebVitals(metric) {
+  
+  console.log(metric);
+}
 
